fd8e63f043353ab085cf6d018710f698a997ad74
ttocsnodrog <68621184+ttocsnodrog@users.noreply.github.com>
2022-05-15 13:01:35 -0600

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -11,7 +11,7 @@ var request = require('request');
 module.exports = NodeHelper.create({
 	// Subclass start method.
 	start: function() {
-		console.log("Started node_helper.js for MMM-DailyBibleVerse-multilang.");
+		console.log("Started node_helper.js for MMM-DailyBibleVerse-multilang2.");
 	},
 
 	socketNotificationReceived: function(notification, payload) {
